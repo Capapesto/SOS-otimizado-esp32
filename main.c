6d43c4aecba01e528893c15c61ad84a3b13d666a
Andrey André <capstolisto@gmail.com>
2026-02-05 09:18:36 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+void pisca_ponto(){
+     gpio_set_level(2,1);
+     printf(".");
+     vTaskDelay(300 / portTICK_PERIOD_MS);
+     gpio_set_level(2,0);
+     vTaskDelay(300 / portTICK_PERIOD_MS);
+}
+
+void pisca_traco(){
+     gpio_set_level(2,1);
+     printf("-");
+     vTaskDelay(700 / portTICK_PERIOD_MS);
+     gpio_set_level(2,0);
+     vTaskDelay(500 / portTICK_PERIOD_MS);
+}
+
+void app_main() {
+  gpio_set_direction(2,GPIO_MODE_OUTPUT);
+  
+  //mensagem
+  while (true) {
+      gpio_set_direction(2,GPIO_MODE_OUTPUT);
+     pisca_ponto();
+     pisca_ponto();
+     pisca_ponto():
+     
+
+     pisca_traco();
+     pisca_traco();
+     pisca_traco();
+
+     pisca_ponto();
+     pisca_ponto();
+     pisca_ponto();
+     vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+}
